35085fd2af755edde23cef49e577a96de4462ce3
toanle312 <toanle3122002@gmail.com>
2022-09-10 20:39:24 +0700

    Upload folder RandomEnglish - the first WPF Program

---
+++ RandomEnglish/RandomEnglish/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomEnglish
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<string> _images;
+        List<string> _vocabularies;
+
+        int _countReset = 0;
+
+        int[] _used = new int[20] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        private void setupData()
+        {
+            _images = new List<string>()
+            {
+                "/Image/animal1.jpg",
+                "/Image/animal2.jpg",
+                "/Image/animal3.jpg",
+                "/Image/animal4.jpg",
+                "/Image/animal5.jpg",
+                "/Image/animal6.jpg",
+                "/Image/animal7.jpg",
+                "/Image/animal8.jpg",
+                "/Image/animal9.jpg",
+                "/Image/animal10.jpg",
+                "/Image/animal11.jpg",
+                "/Image/animal12.jpg",
+                "/Image/animal13.jpg",
+                "/Image/animal14.jpg",
+                "/Image/animal15.jpg",
+                "/Image/animal16.jpg",
+                "/Image/animal17.jpg",
+                "/Image/animal18.jpg",
+                "/Image/animal19.jpg",
+                "/Image/animal20.jpg"
+            };
+
+            _vocabularies = new List<string>()
+            {
+                "Dog (dɒg): Con chó",
+                "Cat (kæt): Con mèo",
+                "Camel (ˈkæməl): Con lạc đà",
+                "Cow (kaʊ): Con bò",
+                "Turkey (ˈtɜːki): Con gà Tây ",
+                "Dove (dəv): Con chim bồ câu",
+                "Duck (dək): Con vịt",
+                "Parrot (pærət): Con vẹt",
+                "Jaguar (ˈjaɡˌwär): Con báo đốm",
+                "Fox (fɒks): Con cáo",
+                "Lion (ˈlaɪən): Con sư tử",
+                "Bear (beə): Con gấu",
+                "Elephant (ˈɛlɪfənt): Con voi",
+                "Squirrel (ˈskwɪrəl): Con sóc",
+                "Porcupine (ˈpɔːkjʊpaɪn): Con nhím",
+                "Hippopotamus (ˌhɪpəˈpɒtəməs): Con hà mã",
+                "Raccoon (rəˈkuːn): Con gấu mèo",
+                "Giraffe (ʤɪˈrɑːf): Con hươu cao cổ",
+                "Rhinoceros (raɪˈnɒsərəs): Con tê giác",
+                "Wolf (wʊlf): Con chó sói"
+            };
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            setupData();
+
+            nextWord();
+        }
+        private void showButton_Click(object sender, RoutedEventArgs e)
+        {
+            nextWord();
+        }
+        private void nextWord()
+        {
+            var rng = new Random();
+
+            int index = rng.Next(0, _images.Count);
+
+            while (_used[index] > 0)
+            {
+                index = rng.Next(0, _images.Count);
+            }
+
+            string vocabulary = _vocabularies[index];
+            vocabularyLabel.Content = vocabulary;
+
+            var bitmap = new BitmapImage(
+                new Uri(_images[index], UriKind.Relative)
+            );
+
+            vocabularyImage.Source = bitmap;
+
+            _used[index]++;
+            _countReset++;
+
+            if(_countReset == 20)
+            {
+                for(int i= 0; i < 20; i++)
+                {
+                    _used[i] = 0;
+                }
+            }
+        }
+    }
+}
---
+++ RandomEnglish/Readme.md
@@ -0,0 +1,12 @@
+#**Lê Phước Toàn - 20120386**
+
+**- Email sinh viên: 20120386@student.hcmus.edu.vn**
+
+**- Email cá nhân: toanle3122002@gmail.com**
+
+**- Chức năng đã hoàn thiện:**
+- Hiển thị ngẫu nhiên một từ vựng và hình ảnh của từ vựng tiếng Anh tương ứng. 
+- Mỗi lần bấm nút đổi (`Show orther words`) thì lựa chọn ngẫu nhiên một từ khác. 
+
+>***- Điểm đề nghị: 10đ***
+
